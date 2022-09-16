073b37f5144a7d2d4237c27224ec7aa1d574f429
kellyy8 <96894568+kellyy8@users.noreply.github.com>
2022-09-16 15:16:24 -0700

    Add files via upload

---
+++ PROJECT4 copy/utility.h
@@ -0,0 +1,16 @@
+//
+//  utility.h
+//  project4
+//
+//  Created by Kelly Yu on 3/6/22.
+//
+
+#ifndef utility_h
+#define utility_h
+
+#include "provided.h"
+
+bool operator<(const AttValPair& a, const AttValPair& b);
+bool operator<(const EmailCount& a, const EmailCount& b);
+
+#endif /* utility_h */
